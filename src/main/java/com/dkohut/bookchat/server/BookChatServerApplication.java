7859449566806a6fb85c6cf559ef80e29413c1aa
Dmytro Kohut <dkohut@chegg.com>
2017-09-18 22:32:29 +0300

    First commit

---
+++ src/main/java/com/dkohut/bookchat/server/BookChatServerApplication.java
@@ -0,0 +1,12 @@
+package com.dkohut.bookchat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookChatServerApplication.class, args);
+	}
+}
